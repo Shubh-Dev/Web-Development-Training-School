72f166d239ee532e74d88212e69df8be823ddb1c
Shubh <shubh.scb@gmail.com>
2022-03-08 21:03:45 +0530

    ADD ARRAY OF OBJ

---
+++ script.js
@@ -0,0 +1,19 @@
+const arrOfImageData = [
+  {
+    headline: 'Our Experts',
+    name1: 'Lila Tretikov',
+    name2: 'Julia Lida',
+    name3: 'Kilnam Chon',
+    name4: 'Yochai Benkler',
+    name5: 'Ryan Merkley',
+    name6: 'SohYeong Noh',
+  },
+  {
+    img1: './images/img-1.jpg',
+    img2: './images/img-2.jpg',
+    img3: './images/img-3.jpg',
+    img4: './images/img-4.jpg',
+    img5: './images/img-5.jpg',
+    img6: './images/img-6.jpg',
+  },
+];
