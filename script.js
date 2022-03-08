2fae94c1338cab31e2f20631aa492c3748032a6f
Shubh <shubh.scb@gmail.com>
2022-03-09 00:36:37 +0530

    THE MOBILE POPUP

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
 const arrOfImageData = [
   {
-    headline: 'Our Experts',
-    name1: 'Lila Tretikov',
-    name2: 'Julia Lida',
-    name3: 'Kilnam Chon',
-    name4: 'Yochai Benkler',
-    name5: 'Ryan Merkley',
-    name6: 'SohYeong Noh',
+    name: 'Yochai Benkler',
+    image: './images/img-1.jpg',
+    designation: 'Anim ad et ullamco sint ad esse aliqua',
+    line: 'orange-bar-mobile',
+    description: 'Qui esse est quis non qui proident et eiusmod ea.Consectetur cillum aliquip occaecat veniam. Proident mollit pariatur nulla Lorem laboris reprehenderit mollit ullamco aute duis adipisicing aute eu.',
   },
   {
-    img1: './images/img-1.jpg',
-    img2: './images/img-2.jpg',
-    img3: './images/img-3.jpg',
-    img4: './images/img-4.jpg',
-    img5: './images/img-5.jpg',
-    img6: './images/img-6.jpg',
+    name: 'Kilnam chon',
+    image: './images/img-2.jpg',
+    designation: 'Anim ad et ullamco sint ad esse aliqua',
+    line: 'orange-bar-mobile',
+    description: 'Qui esse est quis non qui proident et eiusmod ea.Consectetur cillum aliquip occaecat veniam. Proident mollit pariatur nulla Lorem laboris reprehenderit mollit ullamco aute duis adipisicing aute eu.',
+  },
+  {
+    name: 'Ryan Merkley',
+    image: './images/img-3.jpg',
+    designation: 'Anim ad et ullamco sint ad esse aliqua',
+    line: 'orange-bar-mobile',
+    description: 'Qui esse est quis non qui proident et eiusmod ea.Consectetur cillum aliquip occaecat veniam. Proident mollit pariatur nulla Lorem laboris reprehenderit mollit ullamco aute duis adipisicing aute eu.',
+  },
+  {
+    name: 'SohYeong Noh',
+    image: './images/img-4.jpg',
+    designation: 'Anim ad et ullamco sint ad esse aliqua',
+    line: 'orange-bar-mobile',
+    description: 'Qui esse est quis non qui proident et eiusmod ea.Consectetur cillum aliquip occaecat veniam. Proident mollit pariatur nulla Lorem laboris reprehenderit mollit ullamco aute duis adipisicing aute eu.',
+  },
+  {
+    name: 'Julia Leda',
+    image: './images/img-5.jpg',
+    designation: 'Anim ad et ullamco sint ad esse aliqua',
+    line: 'orange-bar-mobile',
+    description: 'Qui esse est quis non qui proident et eiusmod ea.Consectetur cillum aliquip occaecat veniam. Proident mollit pariatur nulla Lorem laboris reprehenderit mollit ullamco aute duis adipisicing aute eu.',
+  },
+  {
+    name: 'Lila Tretikov',
+    image: './images/img-6.jpg',
+    designation: 'Anim ad et ullamco sint ad esse aliqua',
+    line: 'orange-bar-mobile',
+    description: 'Qui esse est quis non qui proident et eiusmod ea.Consectetur cillum aliquip occaecat veniam. Proident mollit pariatur nulla Lorem laboris reprehenderit mollit ullamco aute duis adipisicing aute eu.',
   },
 ];
+const div = document.createElement('div');
+div.className = ('popMobileWindow');
+const crossButton = document.createElement('img');
+crossButton.src = './images/Enabled-2.svg';
+crossButton.className = 'popMobileCrossbutton';
+div.appendChild(crossButton);
+document.getElementById('pop-up-mobile-window').appendChild(div);
+
+const popUpMobileWindow = document.getElementById('hamburger');
+popUpMobileWindow.addEventListener('click', () => {
+  const mobilepopButton = document.querySelector('.popMobileWindow');
+  mobilepopButton.classList.add('active');
+
+  const popOutMobileWindow = document.querySelector('.popMobileCrossbutton');
+  popOutMobileWindow.addEventListener('click', () => {
+    const mobilepopButton = document.querySelector('.popMobileWindow');
+    mobilepopButton.classList.remove('active');
+  });
+});
