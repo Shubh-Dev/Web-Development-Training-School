8904e009eea1d9bd3c9f89bba0c2b53b429e839b
Shubh <shubh.scb@gmail.com>
2022-03-09 17:21:02 +0530

    PAGE STYLE

--- script.js
+++ script.js
@@ -42,13 +42,42 @@ const arrOfImageData = [
     description: 'Qui esse est quis non qui proident et eiusmod ea.Consectetur cillum aliquip occaecat veniam. Proident mollit pariatur nulla Lorem laboris reprehenderit mollit ullamco aute duis adipisicing aute eu.',
   },
 ];
+// image description grid
+const mainImageGrid = document.querySelector('.image-grig');
+mainImageGrid.innerHTML += arrOfImageData.map((project) => (
+  `<div class = 'projects'>
+ <div class = "trainer-image">
+      
+ </div>
+ </div>`
+));
+// mobile-popup-window
 const div = document.createElement('div');
-div.className = ('popMobileWindow');
+div.className = 'popMobileWindow';
 const crossButton = document.createElement('img');
 crossButton.src = './images/Enabled-2.svg';
 crossButton.className = 'popMobileCrossbutton';
 div.appendChild(crossButton);
 document.getElementById('pop-up-mobile-window').appendChild(div);
+
+const homeButton = document.createElement('a');
+homeButton.className = 'home';
+homeButton.href = 'index.html';
+const textNode = document.createTextNode('Home');
+homeButton.appendChild(textNode);
+
+const aboutButton = document.createElement('a');
+aboutButton.className = 'home';
+aboutButton.href = 'about.html';
+const textNode2 = document.createTextNode('About');
+aboutButton.appendChild(textNode2);
+
+const innerDiv = document.createElement('div');
+innerDiv.className = 'inner-div';
+
+innerDiv.appendChild(homeButton);
+innerDiv.appendChild(aboutButton);
+div.appendChild(innerDiv);
 
 const popUpMobileWindow = document.getElementById('hamburger');
 popUpMobileWindow.addEventListener('click', () => {
